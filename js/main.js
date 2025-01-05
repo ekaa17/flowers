e37648a5aac786b3b411e2b1b1c22811abcd1745
ekaa17 <ekasalafi@gmail.com@gmail.com>
2025-01-05 10:31:48 +0700

    first Commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('I LOVE U For You').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
